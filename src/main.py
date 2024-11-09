1edb79dc39e4196a6544de75b15b595a536f1ccc
Sai Mouli Bandari <saimouli4k@gmail.com>
2024-11-09 13:26:42 +0000

    updated controller import

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from src.excel import router
+from src.excel import controller
 
 app = FastAPI()
 
-app.include_router(router=router.router, prefix='/excel', tags=['excel'])
+app.include_router(router=controller.router, prefix='/excel', tags=['excel'])
 
 @app.get("/")
 async def root():
